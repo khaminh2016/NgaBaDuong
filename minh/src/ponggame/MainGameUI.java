d5470b917b0e57106a50a19d398487557d293b98
thu <nguyenthiminhthu.vlu96@gmail.com>
2016-11-08 19:09:33 +0700

    #8 sua diem thanh mau xanh duong

---
+++ minh/src/ponggame/MainGameUI.java
@@ -0,0 +1,38 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ */
+package ponggame;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author Invisible Man
+ *
+ */
+public class MainGameUI extends JFrame{
+	private static final int _HEIGHT = 500;
+	private static final int _WIDTH = 500;
+	private PongPanel pongPanel;
+	
+	public MainGameUI(){
+		setTitle("Pong Game - K21T Ltd.");
+		pongPanel = new PongPanel();
+		setPreferredSize(new Dimension(_WIDTH, _HEIGHT));
+		setLayout(new BorderLayout());
+		getContentPane().add(pongPanel, BorderLayout.CENTER);
+		setLocationRelativeTo(null);
+		this.setResizable(false);
+		pack();
+	}
+
+    public static void main(String[] args) {
+       MainGameUI mainFrame = new MainGameUI();
+       mainFrame.setVisible(true);
+    }
+}
